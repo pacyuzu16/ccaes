1bb443c1d2911e767202f2de28ceff7be4dbf3db
pacyuzu16 <133155388+pacyuzu16@users.noreply.github.com>
2025-05-19 00:50:01 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// Simplified MongoDB connection (deprecated options removed)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+// API routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/complaints', require('./routes/complaints'));
 app.use('/api/users', require('./routes/users'));
